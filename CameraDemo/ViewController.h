cbb4c1a9532f8b3c0592ceb295d9e8174e03f615
Chen Lu <2013luchen@gmail.com>
2015-07-05 18:48:07 -0400

    Initial Commit

---
+++ CameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by 鲁辰 on 7/5/15.
+//  Copyright (c) 2015 ChenLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CameraDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CameraDemo
+//
+//  Created by 鲁辰 on 7/5/15.
+//  Copyright (c) 2015 ChenLu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
